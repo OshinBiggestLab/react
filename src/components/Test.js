d41310ad48eeeffa7c3cae95802924d892830e24
OshinBiggestLab <oshin@biggestlab.io>
2025-06-13 19:58:16 +0700

    Fixed icons issue

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,4 +1,4 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../css/swiper-custom.css";
 
@@ -6,29 +6,44 @@ import "../css/swiper-custom.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+// import "swiper/css/scrollbar";
 import jobDetails from "../data/jobDetails";
 
 function Thumbnails() {
   return (
-    <section className="py-[140px] px-[120px] bg-gray-50 font-poppins">
+    <section className="py-[140px] px-[120px] bg-gray-50 font-poppins relative">
       <div>
-        <h1 className="relative z-20 w-fit text-[#183B56] font-bold text-[40px] leading-[125%] mb-8">
+        <h1 className="relative z-20 w-fit text-[#183B56] font-bold text-[40px] leading-[125%] mb-14">
           Some Popular <br /> Job for You!
         </h1>
+
+        {/* Custom Navigation Buttons */}
+        <div className="absolute top-[220px] left-0 z-10">
+          <span className="custom-prev material-symbols-outlined cursor-pointer">
+            arrow_back_ios
+          </span>
+        </div>
+        <div className="absolute top-[220px] right-0 z-10">
+          <span className="custom-next material-symbols-outlined cursor-pointer">
+            arrow_forward_ios
+          </span>
+        </div>
+
         <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={10} // Gap/Space between slides
-          slidesPerView={3}
-          navigation
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={10}
+          slidesPerView={4}
+          navigation={{
+            prevEl: ".custom-prev",
+            nextEl: ".custom-next",
+          }}
           pagination={{ clickable: true }}
-          // scrollbar={{ draggable: true }}
-          className="max-w-[990px] overflow-scroll"
+          className="rounded-[16.84px]"
         >
           {jobDetails.map((job, index) => (
             <SwiperSlide
               key={index}
-              className="bg-white min-w-[355px] min-h-[292px] rounded-[16.84px] px-6 py-[30px]  shadow-[5px_0px_56.1276px_rgba(55,55,55,0.1098)]"
+              className="bg-white min-w-[355px] min-h-[292px] rounded-[16.84px] px-6 py-[30px] shadow-[5px_0px_56.1276px_rgba(55,55,55,0.1098)]"
             >
               <div className="flex items-center justify-between">
                 <div className="bg-[#fceeea] w-[140px] h-[30px] rounded-[5px] flex items-center justify-center">
@@ -43,7 +58,6 @@ function Thumbnails() {
               <p className="text-[#183B56] max-h-[72px] truncate whitespace-normal mt-[24px] mb-[16px] font-semibold text-[22px] leading-[36px] tracking-[-1px]">
                 {job.position}
               </p>
-              {/* Time & Location */}
               <div className="flex font-dmSans text-[#5A7184] justify-between mb-6">
                 <div className="flex items-center gap-x-2">
                   <img alt="clock" src="images/icons/time.svg" />
--- src/data/jobDetails.js
+++ src/data/jobDetails.js
@@ -35,6 +35,42 @@ const jobDetails = [
     workingDay: "Full Time",
     createdTime: "13h ago",
   },
+  {
+    company: "Umasta",
+    logo: "images/logos/umasta.svg",
+    location: "Tokyo, Japan",
+    category: "Digital Marketing",
+    position: "Associate Product Marketing Consumer Marketing",
+    workingDay: "Part Time",
+    createdTime: "13h ago",
+  },
+  {
+    company: "Carost",
+    logo: "images/logos/carost.svg",
+    location: "Bangalore, India",
+    category: "Copy Writing",
+    position: "Senior Media Content Development",
+    workingDay: "Full Time",
+    createdTime: "14h ago",
+  },
+  {
+    company: "Netflix",
+    logo: "images/logos/netflix.svg",
+    location: "London, UK",
+    category: "UX Design",
+    position: "UX/UI Designer expert in Mobile App, Web Sites and Softwares",
+    workingDay: "Part Time",
+    createdTime: "13h ago",
+  },
+  {
+    company: "Vimo",
+    logo: "images/logos/vimo.svg",
+    location: "Tokyo, Japan",
+    category: "Music",
+    position: "Audio Production and Post-Production Services for ia",
+    workingDay: "Full Time",
+    createdTime: "13h ago",
+  },
 ];
 
 export default jobDetails;
